a9a11f70c6c0dacdc89ad7b47d868603cc7039a4
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-04-27 09:45:10 +0200

    Added some comments in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+// Da tenere attivo anche quando si visualizzano le altre pagine
 import "./App.css"
-import "./Settings.css"
+// Da tenere attivo solo per la pagina settings
+// import "./Settings.css"
+// Da tenere attivo solo per la pagina Profile
 // import "./Profile.css"
 import Footer from "./components/Footer"
 import HomePage from "./components/HomePage"
@@ -16,8 +19,10 @@ function App() {
         <MyNavBar />
       </header>
       <Container>
-        <Settings />
-        {/* <HomePage /> */}
+        <HomePage />
+        {/* Da tenere attivo solo per la pagina settings */}
+        {/* <Settings /> */}
+        {/* Da tenere attivo solo per la pagina Profile */}
         {/* <Profile /> */}
         <Footer />
       </Container>
